9e194cc736cff0999f076fbfaf6bc33827fa6664
Yangkai He <yh952@cornell.edu>
2022-06-20 17:09:24 -0400

    Reinitialized VUE 2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
 import router from './router'
 
-const app = createApp(App)
+Vue.config.productionTip = false
 
-app.use(router)
-
-app.mount('#app')
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
